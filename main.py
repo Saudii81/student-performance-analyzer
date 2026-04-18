256ef8ce84c742645d4bc6a3d3a3232c96c5e26e
Saudii <isaudatu81@gmail.com>
2026-04-18 15:11:40 +0100

    add a comment to clarify the main entry point of the application

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from app.utils import load_students_from_json
 from app.cli import run_cli
 
+# Main entry point for the application
 if __name__ == "__main__":
     students = load_students_from_json("data/sample_students.json")
     run_cli(students)
